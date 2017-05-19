fd1029f8a99b0a057db7a14361d41d2a36f08185
nickg <nickg@signalsciences.com>
2017-05-18 19:39:41 -0700

    attempt parallelization

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path"
@@ -8,6 +9,51 @@ import (
 
 // over-ridden via goreleaser
 var version = "master"
+
+// single upload, no channels, no nothing
+func upload(s3conf *S3PushConfig, up FileStat) error {
+	ma := s3conf.Match(up.Name)
+	if ma == nil {
+		return fmt.Errorf("Want to upload %q but got no HTTP conf", up.Name)
+	}
+	upi, err := NewUploadInput(ma)
+	if err != nil {
+		return fmt.Errorf("Error configuring %q: %s", up.Name, err)
+	}
+	upi.Bucket = &s3conf.Bucket
+	key := path.Join("", up.Name)
+	upi.Key = &key
+	if upi.ContentType == nil && up.Mime != "" {
+		upi.ContentType = &up.Mime
+	}
+	log.Printf("%s: configuring: %s", up.Name, DumpUploadInput(upi))
+	log.Printf("PATH= %s", up.Path)
+	fd, err := os.Open(up.Path)
+	defer func() { _ = fd.Close() }()
+
+	if err != nil {
+		return fmt.Errorf("Unable to open %s", err)
+	}
+	upi.Body = fd
+	// TODO: UploaderOutput
+	_, err = s3conf.s3uploader.Upload(upi)
+	if err != nil {
+		return fmt.Errorf("Unable to upload %s: %s", up.Name, err)
+	}
+	return nil
+}
+
+func worker(id int, conf *S3PushConfig, jobs <-chan FileStat, results chan<- error) {
+	for up := range jobs {
+		err := upload(conf, up)
+		if err != nil {
+			log.Printf("worker: %d: finished", id)
+		} else {
+			log.Printf("worker %d errors: %s", id, err)
+		}
+		results <- err
+	}
+}
 
 func main() {
 	confName := ".s3push.sh"
@@ -26,36 +72,29 @@ func main() {
 	log.Printf("Uploads: %d", len(uploads))
 	log.Printf("Downloads: %d", len(downloads))
 
-	for _, up := range uploads {
-		ma := s3conf.Match(up.Name)
-		if ma == nil {
-			log.Printf("Want to upload %q but got no HTTP conf", up.Name)
-			continue
-		}
-		upi, err := NewUploadInput(ma)
-		if err != nil {
-			log.Fatalf("Error configuring %q: %s", up.Name, err)
-		}
-		upi.Bucket = &s3conf.Bucket
-		key := path.Join("", up.Name)
-		upi.Key = &key
-		if upi.ContentType == nil && up.Mime != "" {
-			upi.ContentType = &up.Mime
-		}
-		log.Printf("%s: configuring: %s", up.Name, DumpUploadInput(upi))
-		log.Printf("PATH= %s", up.Path)
-		fd, err := os.Open(up.Path)
-		defer func() { _ = fd.Close() }()
+	jobs := make(chan FileStat, len(uploads))
+	results := make(chan error, len(uploads))
 
-		if err != nil {
-			log.Printf("Unable to open %s", err)
-			continue
-		}
-		upi.Body = fd
-		result, err := s3conf.s3uploader.Upload(upi)
-		if err != nil {
-			log.Printf("Unable to upload %s: %s", up.Name, err)
-		}
-		log.Printf("Uploaded %s", result.Location)
+	// unlease some workers
+	for w := 0; w < 1; w++ {
+		go worker(w, s3conf, jobs, results)
 	}
+
+	// push into queue
+	for _, up := range uploads {
+		jobs <- up
+	}
+
+	// all done
+	close(jobs)
+
+	// get results
+	errcount := 0
+	for a := 0; a < len(uploads); a++ {
+		err := <-results
+		if err != nil {
+			errcount++
+		}
+	}
+	log.Printf("Total Errors: %d", errcount)
 }
