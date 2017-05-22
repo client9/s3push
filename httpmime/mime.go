3f26b2938ea9976ea95f02b7853a8107876818c8
nickg <nickg@signalsciences.com>
2017-05-22 11:34:42 -0700

    add doco and cleanup api

--- httpmime/mime.go
+++ httpmime/mime.go
@@ -5,18 +5,22 @@ import (
 	"os"
 )
 
-var typeFiles = []string{
+// TypeFiles is the list of files used by golang to load mime types.
+//
+// See https://golang.org/pkg/mime/#TypeByExtension
+//
+var TypeFiles = []string{
 	"/etc/mime.types",
 	"/etc/apache2/mime.types",
 	"/etc/apache/mime.types",
 }
 
-func Init() {
-	for k, v := range mimeTypes {
-		_ = mime.AddExtensionType(k, v)
-	}
-}
 
+// MimeTypeFiles returns a list of default mime files found
+// by golang's native loader.  This is done by inspection
+// and may or may not represent what was actually loaded or
+// if what order.
+//
 func MimeTypeFiles() []string {
 	out := []string{}
 	for _, fname := range typeFiles {
@@ -25,4 +29,28 @@ func MimeTypeFiles() []string {
 		}
 	}
 	return out
+}
+
+// IsLoaded returns true if the internal golang mapping
+// appears to be sane.
+func IsLoaded() bool {
+	// golang's internal table does not include .txt
+	// if this is missing something is wrong.
+	return mime.TypeByExtension(".txt") != ""
+}
+
+// AddDefaults adds a standard set of extensions and mime-types to
+//  golang's mime package.  This may over-ride existing configuration.
+func AddDefaults() {
+	for k, v := range mimeTypes {
+		_ = mime.AddExtensionType(k, v)
+	}
+}
+
+// Init attempts to make sure golang's mime package is initialized
+// correctly on platforms missing basic mime type files
+func Init() {
+	if !IsLoader() {
+		AddDefaults()
+	}
 }
